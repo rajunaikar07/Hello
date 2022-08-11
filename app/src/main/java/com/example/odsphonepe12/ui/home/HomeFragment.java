c6520f5d05e1426babbaf2fdee2b4c7d09a896ac
rajunaikar07 <110777070+rajunaikar07@users.noreply.github.com>
2022-08-11 17:56:20 +0530

    raju sanjay

--- app/src/main/java/com/example/odsphonepe12/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/odsphonepe12/ui/home/HomeFragment.java
@@ -9,6 +9,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -116,11 +117,7 @@ public class HomeFragment extends Fragment {
 
 
         return root;
-
-
     }
-
-
     private void loadFragment(RechargeFragment rechargeFragment) {
 //        FragmentManager fm=getFragmentManager();
 //        FragmentTransaction veriabelname=fm.beginTransaction();
